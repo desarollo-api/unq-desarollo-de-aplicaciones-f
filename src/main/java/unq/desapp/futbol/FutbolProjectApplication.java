c64660174e7a357ce3bf4a23bf6e34ad594232c4
Gino Statello <ginostatello@MacBook-Pro-de-Gino.local>
2025-08-31 20:33:57 -0300

    Initial commit: Proyecto de fútbol con Spring Boot

---
+++ src/main/java/unq/desapp/futbol/FutbolProjectApplication.java
@@ -0,0 +1,13 @@
+package unq.desapp.futbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unq/desapp/futbol/FutbolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package unq.desapp.futbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
