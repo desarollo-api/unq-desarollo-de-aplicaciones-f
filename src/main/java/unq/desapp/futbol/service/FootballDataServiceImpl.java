e3e5a221d7a13d73fddfba159803debdf782e454
Rodrigo Caceres <rodrigo.daniel.caceres.20@gmail.com>
2025-09-20 21:35:49 -0300

    deleted player info endpoint

--- src/main/java/unq/desapp/futbol/service/FootballDataServiceImpl.java
+++ src/main/java/unq/desapp/futbol/service/FootballDataServiceImpl.java
@@ -5,16 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import unq.desapp.futbol.model.Player;
-import unq.desapp.futbol.model.Team;
-import unq.desapp.futbol.model.TeamDetails;
-import unq.desapp.futbol.model.TeamListResponse;
 
 @Service
 public class FootballDataServiceImpl implements FootballDataService {
-
     private static final Logger logger = LoggerFactory.getLogger(FootballDataServiceImpl.class);
     private final WebClient webClient;
     private final ScrapingService scrapingService;
@@ -31,39 +25,7 @@ public class FootballDataServiceImpl implements FootballDataService {
     }
 
     @Override
-    public Mono<Player> getPlayerById(Long id) {
-        return this.webClient.get()
-                .uri("/persons/{id}", id)
-                .retrieve()
-                .bodyToMono(Player.class)
-                .flatMap(this::enrichPlayerWithRating)
-                .doOnError(error -> logger.error("Error fetching player data for id {}: {}", id, error.getMessage()));
-    }
-
-    @Override
-    public Flux<Team> getTeams() {
-        return this.webClient.get()
-                .uri("/teams")
-                .retrieve()
-                .bodyToMono(TeamListResponse.class)
-                .flatMapMany(response -> Flux.fromIterable(response.getTeams()))
-                .doOnError(error -> logger.error("Error fetching teams: {}", error.getMessage()));
-    }
-
-    @Override
-    public Mono<TeamDetails> getTeamById(Long teamId) {
-        return this.webClient.get()
-                .uri("/teams/{id}", teamId)
-                .retrieve()
-                .bodyToMono(TeamDetails.class);
-    }
-
-    private Mono<Player> enrichPlayerWithRating(Player player) {
-        return scrapingService.getPlayerRating(player.getName())
-                .map(rating -> {
-                    player.setRating(rating);
-                    return player;
-                })
-                .defaultIfEmpty(player); // Si no se encuentra rating, devuelve el jugador original
+    public Mono<String> getTeamSquad(String teamName, String country) {
+        return scrapingService.findTeamPage(teamName, country);
     }
 }
