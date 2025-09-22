53a2d597979d34d072da06900654d1ae8ed4d7e4
Rodrigo Caceres <rodrigo.daniel.caceres.20@gmail.com>
2025-09-21 21:08:22 -0300

    added TeamController and Player tests

---
+++ src/test/java/unq/desapp/futbol/TeamControllerTest.java
@@ -0,0 +1,57 @@
+package unq.desapp.futbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import unq.desapp.futbol.model.Player;
+import unq.desapp.futbol.service.FootballDataService;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class TeamControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private FootballDataService footballDataService;
+
+    @Test
+    void testGetSquadFromScraping_whenTeamExists_returnsOkWithSquad() {
+        String country = "Argentina";
+        String teamUrlName = "Boca-Juniors";
+        String teamName = "Boca Juniors";
+        Player player = new Player("Edinson Cavani", 37, "Uruguay", "Forward", 7.5, 30, 20, 5, 0, 2);
+        List<Player> squad = Collections.singletonList(player);
+
+        when(footballDataService.getTeamSquad(teamName, country)).thenReturn(Mono.just(squad));
+
+        webTestClient.get().uri("/teams/{country}/{name}/squad", country, teamUrlName)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Player.class)
+                .isEqualTo(squad);
+    }
+
+    @Test
+    void testGetSquadFromScraping_whenTeamDoesNotExist_returnsNotFound() {
+        String country = "Nowhere";
+        String teamUrlName = "Non-Existent-Team";
+        String teamName = "Non Existent Team";
+
+        when(footballDataService.getTeamSquad(teamName, country)).thenReturn(Mono.empty());
+
+        webTestClient.get().uri("/teams/{country}/{name}/squad", country, teamUrlName)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+}
