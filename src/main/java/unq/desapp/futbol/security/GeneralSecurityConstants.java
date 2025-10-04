e0edbde3ca48e691c70669a412c7d7fd09930448
francoNeira <neira.franco.ariel@gmail.com>
2025-10-04 02:07:34 -0300

    Add public modifier to constants

--- src/main/java/unq/desapp/futbol/security/GeneralSecurityConstants.java
+++ src/main/java/unq/desapp/futbol/security/GeneralSecurityConstants.java
@@ -7,31 +7,31 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-final class GeneralSecurityConstants {
+public final class GeneralSecurityConstants {
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    final class Auth {
-        static final String PATTERN = "/auth/**";
+    public final class Auth {
+        public static final String PATTERN = "/auth/**";
     }
 
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    final class Cors {
-        static final List<String> ALL_ALLOWED = List.of("*");
-        static final List<String> ALLOWED_METHODS = List.of(
+    public final class Cors {
+        public static final List<String> ALL_ALLOWED = List.of("*");
+        public static final List<String> ALLOWED_METHODS = List.of(
             HttpMethod.GET.name(),
             HttpMethod.POST.name(),
             HttpMethod.PUT.name(),
             HttpMethod.DELETE.name(),
             HttpMethod.PATCH.name(),
             HttpMethod.OPTIONS.name());
-        static final List<String> EXPOSED_HEADERS = List.of(
+        public static final List<String> EXPOSED_HEADERS = List.of(
             HttpHeaders.AUTHORIZATION,
             HttpHeaders.CONTENT_TYPE);
-        static final String PATTERN = "/**";
+        public static final String PATTERN = "/**";
     }
 
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    final class Jwt {
-        static final String DEFAULT = "";
-        static final String PREFIX = "Bearer ";
+    public final class Jwt {
+        public static final String DEFAULT = "";
+        public static final String PREFIX = "Bearer ";
     }
 }
