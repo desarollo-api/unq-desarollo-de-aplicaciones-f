d287d26f5defe638af2cb0e039adfae274035771
Rodrigo Caceres <rodrigo.daniel.caceres.20@gmail.com>
2025-10-08 20:53:35 -0300

    added swagger-ui to public URLs

--- src/main/java/unq/desapp/futbol/service/FootballDataServiceImpl.java
+++ src/main/java/unq/desapp/futbol/service/FootballDataServiceImpl.java
@@ -1,10 +1,7 @@
 package unq.desapp.futbol.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import unq.desapp.futbol.model.Player;
 
@@ -12,8 +9,6 @@ import java.util.List;
 
 @Service
 public class FootballDataServiceImpl implements FootballDataService {
-    private static final Logger logger = LoggerFactory.getLogger(FootballDataServiceImpl.class);
-    private final WebClient webClient;
     private final ScrapingService scrapingService;
 
     public FootballDataServiceImpl(ScrapingService scrapingService,
@@ -21,10 +16,6 @@ public class FootballDataServiceImpl implements FootballDataService {
                                    @Value("${football.api.token}") String apiToken
                                    ) {
         this.scrapingService = scrapingService;
-        this.webClient = WebClient.builder()
-                .baseUrl(baseUrl)
-                .defaultHeader("X-Auth-Token", apiToken)
-                .build();
     }
 
     @Override
