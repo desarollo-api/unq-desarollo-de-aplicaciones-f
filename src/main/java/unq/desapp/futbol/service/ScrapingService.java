5def08586e309bddd6dbf1796a4ff183ac3fd505
francoNeira <neira.franco.ariel@gmail.com>
2025-10-12 23:06:24 -0300

    Add upcoming matches function to interface protocols

--- src/main/java/unq/desapp/futbol/service/ScrapingService.java
+++ src/main/java/unq/desapp/futbol/service/ScrapingService.java
@@ -1,10 +1,12 @@
 package unq.desapp.futbol.service;
 
 import reactor.core.publisher.Mono;
+import unq.desapp.futbol.model.Match;
 import unq.desapp.futbol.model.Player;
 
 import java.util.List;
 
 public interface ScrapingService {
     Mono<List<Player>> getTeamSquad(String teamName, String country);
+    Mono<List<Match>> getUpcomingMatches(String teamName, String country);
 }
