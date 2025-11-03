33adedeae0752170a8b0a722b8a59eb1a45d495f
francoNeira <neira.franco.ariel@gmail.com>
2025-11-03 02:20:13 -0300

    Remove unused imports

--- src/main/java/unq/desapp/futbol/model/Match.java
+++ src/main/java/unq/desapp/futbol/model/Match.java
@@ -1,7 +1,5 @@
 package unq.desapp.futbol.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
