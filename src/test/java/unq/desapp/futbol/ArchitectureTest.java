081ff3b615255553d813654dc0dc5c139fb81278
francoNeira <neira.franco.ariel@gmail.com>
2025-11-29 22:14:19 -0300

    Test: Add layered architecture rules

--- src/test/java/unq/desapp/futbol/ArchitectureTest.java
+++ src/test/java/unq/desapp/futbol/ArchitectureTest.java
@@ -6,6 +6,8 @@ import com.tngtech.archunit.lang.ArchRule;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
 @AnalyzeClasses(packages = "unq.desapp.futbol")
 public class ArchitectureTest {
 
@@ -13,5 +15,16 @@ public class ArchitectureTest {
     static final ArchRule sanity_check = classes()
             .that().resideInAPackage("unq.desapp.futbol")
             .should().resideInAPackage("unq.desapp.futbol");
+
+    @ArchTest
+    static final ArchRule layered_architecture = layeredArchitecture()
+            .consideringOnlyDependenciesInAnyPackage("unq.desapp.futbol..")
+            .layer("Controller").definedBy("..webservice..")
+            .layer("Service").definedBy("..service..")
+            .layer("Repository").definedBy("..repository..")
+            .layer("Security").definedBy("..security..")
+            .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
+            .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller", "Security")
+            .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service");
 
 }
